e83b7a5f33f012940960ac3af6457e78d1926af5
王蕾 <wanglei@wangleideMacBook-Pro.local>
2017-02-09 10:42:16 +0800

    http

--- nodejs/http/http_server_post.js
+++ nodejs/http/http_server_post.js
@@ -14,25 +14,3 @@ http.createServer(function (req, res) {
     res.end(JSON.stringify(resObj));
   });
 }).listen(6000);
-
-
-var http = require('http');
-var options = {
-  host: '127.0.0.1',
-  path: '/',
-  port: '6000',
-  method: 'POST'
-};
-function readJSONResponse(respose) {
-  var resposeData = '';
-  respose.on('data',function (chunk) {
-    resposeData += chunk;
-  });
-  respose.on('end',function() {
-    var dataObj = JSON.parse(resposeData);
-    console.log(dataObj);
-  });
-}
-var req = http.request(options, readJSONResponse);
-req.write('{"name":"wanglei","occupation":"burglar"}');
-req.end();
